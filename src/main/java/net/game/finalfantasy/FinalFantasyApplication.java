8bf3cb2182bab8aafcb84945adc1122c5c3b4e97
peter.chao <peter.chao@ibm.com>
2025-06-29 02:31:53 +0800

    update .gitignore

--- src/main/java/net/game/finalfantasy/FinalFantasyApplication.java
+++ src/main/java/net/game/finalfantasy/FinalFantasyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinalfantasyApplication {
+public class FinalFantasyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FinalfantasyApplication.class, args);
+        SpringApplication.run(FinalFantasyApplication.class, args);
     }
 
 }
